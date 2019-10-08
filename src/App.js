ce8192685d30932f18a3a43191a3daf03eafa242
Soulskiez <tylersolnikowski@gmail.com>
2019-10-08 13:24:15 -0400

    Added mui and prettier, removed App.css and followed mui styles pattern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { makeStyles, mergeClasses } from '@material-ui/styles'
+import logo from './logo.svg'
+
+const useStyles = makeStyles({
+  root: {
+    textAlign: 'center',
+  },
+  appHeader: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white',
+  },
+  appLogo: {
+    height: '40vmin',
+  },
+  appLink: {
+    color: '#09D3AC',
+  },
+})
 
 function App() {
-  
-  const [tyler, setTyler] = useState('tyler');
-
+  const [tyler, setTyler] = useState('tyler')
+  const classes = useStyles()
   const changeToKristina = () => {
-    setTyler('kristina');
+    setTyler('kristina')
   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.root}>
+      <header className={classes.appHeader}>
+        <img src={logo} className={classes.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{tyler}</p>
-        <button onClick={changeToKristina}>click this button to change tyler ^ to kristina</button>
+        <button onClick={changeToKristina}>
+          click this button to change tyler ^ to kristina
+        </button>
         <a
-          className="App-link"
+          className={classes.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -28,7 +51,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
